62608043bd6dd1d1d70f712dc4b11a6c9a9681c6
Tu Nguyen <tunguyen@Tus-MacBook-Pro.local>
2019-06-08 11:29:12 -0400

    Almost Finish except paypal

---
+++ build/precache-manifest.5b7b6b8ef69452a2c6bc12eb0fd7fb1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76040095c717b27cab21656ba29f6a6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e429c1772902fdf14a50",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "819e06717e22cfd99680",
+    "url": "/static/css/main.f50a1ff2.chunk.css"
+  },
+  {
+    "revision": "e429c1772902fdf14a50",
+    "url": "/static/js/2.1d66cb81.chunk.js"
+  },
+  {
+    "revision": "819e06717e22cfd99680",
+    "url": "/static/js/main.17aba658.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
